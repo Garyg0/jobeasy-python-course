b94e0322b0e27a1d461b2e886c616efb2290c588
GaryG0 <mist3fam3@gmail.com>
2020-08-19 13:01:44 -0400

    homework 1

--- lesson_1/homework_1_5.py
+++ lesson_1/homework_1_5.py
@@ -1,11 +1,11 @@
 # Change result_string_1 that 'very simple language' will be displayed on a new line
 
-result_string_1 = 'Python is the very simple language'
+result_string_1 = 'Python is the \n very simple language'
 
 
 # Change result_string_2 to print out the phrase: 'What does the word 'integer' mean'
 
-result_string_2 = 'What does the word 'integer' mean'
+result_string_2 = "What does the word \'integer\' mean"
 
 
 # Assign number variable to value "5" (as a string). Then rise the number to the power 3.
